ef64f0aa0c64a8553821f1e7467aa90c135c8923
MDIsrar <Mohammadisrar1010@gmail.com>
2024-04-16 16:44:40 +0530

    deployment ready

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,26 @@ app.use("/api/user", userRouter)
 app.use("/api/chat", chatRouter)
 app.use("/api/message", messageRouter)
 
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    );
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running..");
+    });
+}
+
+// --------------------------deployment------------------------------
+
+
 app.use(notFound)
 app.use(errorHandler)
 
